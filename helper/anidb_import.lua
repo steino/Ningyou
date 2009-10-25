1ddd7e3421a45927a9d0bfb2e903261958b36043
Stein-Ivar <stein-ivar@berghei.no>
2009-10-25 22:56:39 +0100

    [anidb import] added a update part

--- helper/anidb_import.lua
+++ helper/anidb_import.lua
@@ -1,11 +1,30 @@
 require'helper.mysql'
 local titles = loadfile(os.getenv("HOME").."/animeTitles.lua")()
 
-local query = _DB:prepare('insert into nin_data_anime (id, title, official_title) values (?,?,?)')
+local function check_anime(id)
+	local check = _DB:prepare('select id from nin_data_anime where id = ?')
+	check:execute(id)
+	if check:fetch() then
+		return true
+	else 
+		return nil
+	end
+end
+
+local insert = _DB:prepare('insert into nin_data_anime (id, title, official_title) values (?,?,?)')
+local update = _DB:prepare("update nin_data_anime set id = '?', title = '?', official_title = '?'")
+
 
 for i, k in pairs(titles) do
-	query:execute(tonumber(i), k["title"], k[1])
+	if check_anime(i) then
+		update:execute(tonumber(i), k["title"], k[1])
+	else
+		insert:execute(tonumber(i), k["title"], k[1])
+	end
 end
 
 _DB:commit()
 query:close()
+
+io.write("Inserted ".. insert:affected().." rows.")
+io.write("Updated " .. update:affected().." rows.")
