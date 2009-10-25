2729c1d8f259a6289615aa60d18fb5713fe700eb
Stein-Ivar <stein-ivar@berghei.no>
2009-10-25 22:28:14 +0100

    [anidb import] basic import of anidb titles into mysql

---
+++ helper/anidb_import.lua
@@ -0,0 +1,11 @@
+require'helper.mysql'
+local titles = loadfile(os.getenv("HOME")../animeTitles.lua)
+
+local query = _DB:prepare('insert into nin_data_anime (id, title, official_title) values (?,?,?)')
+
+for i, k in pairs(titles) do
+	query:execute(tonumber(i), k["title"], k[1])
+end
+
+_DB:commit()
+query:close()
