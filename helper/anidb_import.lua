9cf6502edec34d1712ba2d1506a8db1bff007cfa
Stein-Ivar <stein-ivar@berghei.no>
2009-10-26 13:22:24 +0100

    [anidb import] some things just doesnt need to be local, and more manual stuff

--- helper/anidb_import.lua
+++ helper/anidb_import.lua
@@ -2,7 +2,7 @@ require'helper.mysql'
 local titles = loadfile(os.getenv("HOME").."/animeTitles.lua")()
 local up, ins = 0, 0
 local updates = {}
-
+local update_count = 0
 
 local function check_anime()
 	local check = _DB:prepare('select id from nin_data_anime')
@@ -11,6 +11,7 @@ local function check_anime()
 	for row in check:rows(true) do
 		if titles[row["id"]] then
 			table.insert(updates, row["id"], "true")
+			update_count = update_count + 1
 		end
 	end
 	check:close()
@@ -24,15 +25,14 @@ check_anime()
 for i, k in pairs(titles) do
 	if updates[tonumber(i)] then
 		update:execute(k["title"], k[1], tonumber(i))
-		local up = up + 1
+		up = up + 1
 	else
 		insert:execute(tonumber(i), k["title"], k[1])
-		local ins = ins + 1
+		ins = ins + 1
 	end
 end
 
 _DB:commit()
-print("There is " .. #updates .. " titles in the update table")
 print("Inserted ".. ins .." rows.")
 print("Updated " .. up .." rows.")
 
@@ -40,3 +40,4 @@ insert:close()
 update:close()
 up, ins = nil, nil
 updates = nil
+update_count = nil
