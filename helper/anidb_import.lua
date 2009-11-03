ad12c66cd409ebc8c2f5398ebd46b1ac28a6e209
Stein-Ivar <stein-ivar@berghei.no>
2009-11-03 08:37:00 +0100

    [anidb import] small cleanup

--- helper/anidb_import.lua
+++ helper/anidb_import.lua
@@ -4,23 +4,21 @@ local up, ins = 0, 0
 local updates = {}
 local update_count = 0
 
-local function check_anime()
-	local check = _DB:prepare('select id from nin_data_anime')
-	check:execute()
-	_DB:commit()
-	for row in check:rows(true) do
-		if titles[row["id"]] then
-			updates[row["id"]] = "true"
-			update_count = update_count + 1
-		end
-	end
-	check:close()
-end
-
+local check = _DB:prepare('select id from nin_data_anime')
 local insert = _DB:prepare('insert into nin_data_anime (id, title, official_title) values (?,?,?)')
 local update = _DB:prepare("update nin_data_anime set title = '?', official_title = '?' where id = ?")
 
-check_anime()
+check:execute()
+_DB:commit()
+
+for row in check:rows(true) do
+	if titles[row["id"]] then
+		updates[row["id"]] = "true"
+		update_count = update_count + 1
+	end
+end
+
+check:close()
 
 for id, t in pairs(titles) do
 	if updates[id] then
