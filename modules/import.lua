8349b993849430a9635213eb788e1cc4fe96568f
Stein-Ivar Berghei <stein-ivar@berghei.no>
2009-11-07 22:44:27 +0100

    [import] fixing small error

--- modules/import.lua
+++ modules/import.lua
@@ -63,7 +63,7 @@ local function myanimelist(userid, file)
 					if not run_update then print("Error updating "..title..": " ..error_update) end
 				else
 					run_importi, error_import = import:execute(userid, animeid, categoryid, episodes)
-					if not run import then print("Error adding "..title..": " ..error_import) end
+					if not run_import then print("Error adding "..title..": " ..error_import) end
 				end
 			else
 				print"Anime "..title.." not found"
