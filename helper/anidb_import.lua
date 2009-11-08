3a73d616c3464f360d5969c8c05e85c77085afca
Stein-Ivar Berghei <stein-ivar@berghei.no>
2009-11-08 13:43:05 +0100

    [anidb import] slight rewrite and fix

--- helper/anidb_import.lua
+++ helper/anidb_import.lua
@@ -5,7 +5,7 @@ local updates = {}
 local update_count = 0
 local run_update, error_update, run_import, error_import
 
-local check = _DB:prepare('select id from nin_data_anime')
+local check = _DB:prepare('select animeid, language from nin_titles_anime_anidb')
 local import = _DB:prepare('insert into nin_titles_anime_anidb (id, title, language) values (?,?,?)')
 local update = _DB:prepare("update nin_titles_anime_anidb set title = '?', language = '?' where animeid = ?")
 
@@ -13,8 +13,8 @@ check:execute()
 _DB:commit()
 
 for row in check:rows(true) do
-	if titles[row["id"]] then
-		updates[row["id"]] = "true"
+	if titles[row["animeid"]][row["langage"]] then
+		updates[row["animeid"]["language"]] = "true"
 		update_count = update_count + 1
 	end
 end
@@ -22,15 +22,13 @@ end
 check:close()
 
 for id, t in pairs(titles) do
-	if updates[id] then
-		for lang, title in pairs(t) do
+	for lang, title in pairs(t) do
+		if updates[id][lang] then
 			run_update, error_update = update:execute(title, lang, id)
-			if not run_update then print("Could not update \"" .. title .. "\": ".. error_update) end
-		end
-	else
-		for lang, title in pairs(t) do
+			if not run_update then print("Could not update \"" .. title .. "\": ".. error_update) else up = up + 1 end
+		else
 			run_import, error_import = import:execute(id, title, lang)
-			if not run_update then print("Could not import \"" .. title .. "\": ".. error_import) end
+			if not run_update then print("Could not import \"" .. title .. "\": ".. error_import) else ins = ins + 1 end
 		end
 	end
 end
