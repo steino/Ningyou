eb5c827b9e3d1681b84c6f7be8ee9e391064629d
Stein-Ivar Berghei <stein-ivar@berghei.no>
2009-11-08 13:49:26 +0100

    [anidb import] fixing the updates table

--- helper/anidb_import.lua
+++ helper/anidb_import.lua
@@ -13,7 +13,8 @@ check:execute()
 _DB:commit()
 
 for row in check:rows(true) do
-	if titles[row["animeid"]][row["langage"]] then
+	if titles[row["animeid"]][row["language"]] then
+		if not updates[row["animeid"] then updates[row["animeid"] = {} end
 		updates[row["animeid"]][row["language"]] = "true"
 		update_count = update_count + 1
 	end
