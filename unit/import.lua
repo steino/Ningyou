d9f890f052b2138dd9e5f5e9ad3cff1a64840125
Stein-Ivar Berghei <stein-ivar@berghei.no>
2009-11-13 10:18:05 +0100

    [unit] completing the unit test for import

--- unit/import.lua
+++ unit/import.lua
@@ -1,10 +1,13 @@
 local import = require"modules.import"
 
-local userid = 2
-local file = os.getenv("HOME").."/animelist.xml"
+local mal_userid = 2
+local mal_file = os.getenv("HOME").."/animelist.xml"
 
-local mal_ins, mal_up, notfound = import.myanimelist(userid, file)
-local anidb_ins, anidb_up = import.anidb(userid, file)
+local anidb_userid = 0
+local anidb_file = os.getenv("HOME").."/anidb.xml"
+
+local mal_ins, mal_up, notfound = import.myanimelist(mal_userid, mal_file)
+local anidb_ins, anidb_up = import.anidb(anidb_userid, anidb_file)
 
 print("MyAnimeList: " .. mal_ins .. " inserts. " .. mal_up .. " updates.")
 print("AniDB: " .. anidb_ins .. " inserts. " .. anidb_up .. " updates.")
