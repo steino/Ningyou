086c15045a8e593107d59f465d3d254bf87ffc4b
Trond A Ekseth <troeks@gmail.com>
2010-03-09 14:42:39 +0100

    [dispatcher] dump some CGI vars so we know what we're dealing with.

--- index.lua
+++ index.lua
@@ -10,5 +10,28 @@ end
 -- Guess we should move this into the dispatcher.
 wln("Content-Type: text/html", "\r\n")
 
-pcall(w, "Swoosh Ningyou~~")
-pcall(w, _ENV"PATH_INFO")
+pcall(w, "Swoosh Ningyou~~<br />")
+for i, var in ipairs{
+	'AUTH_TYPE',
+	'CONTENT_LENGTH',
+	'CONTENT_TYPE',
+	'GATEWAY_INTERFACE',
+	'PATH_INFO',
+	'PATH_TRANSLATED',
+	'QUERY_STRING',
+	'REMOTE_ADDR',
+	'REMOTE_HOST',
+	'REMOTE_IDENT',
+	'REMOTE_USER',
+	'REQUEST_METHOD',
+	'SCRIPT_NAME',
+	'SCRIPT_FILENAME',
+	'SCRIPT_URL',
+	'SCRIPT_URI',
+	'SERVER_NAME',
+	'SERVER_PORT',
+	'SERVER_PROTOCOL',
+	'SERVER_SOFTWARE',
+} do
+	print(string.format('%02d: %s = %s<br />',i, var, _ENV(var) or 'nil'))
+end
