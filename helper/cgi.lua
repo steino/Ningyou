634ef3aef8353ba86884382c7ec33e7ca995bb36
Stein-Ivar Berghei <stein-ivar@berghei.no>
2010-03-29 19:18:59 +0200

    [cgi] add a parser for post request method.

---
+++ helper/cgi.lua
@@ -0,0 +1,22 @@
+return {
+	Post = function(self, stdin, limit)
+		limit = tonumber(limit) or 0
+		local blocksize = 2048
+		if limit < 1 then
+			stdin:close()
+			return nil
+		else
+			local read = (limit > blocksize) and blocksize or limit
+			limit = limit - read
+			local args = stdin:read(read)
+			if not args then
+				stdin:close()
+				return nil
+			else
+				local out = {}
+				args:gsub("([^&=]+)=([^&=]*)&?", function(i,v) out[i] = v end)i
+				return out
+			end
+		end
+	end,
+}
