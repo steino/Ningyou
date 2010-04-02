cc52bfe9a3aca1e4db93583598d3893889791657
Stein-Ivar Berghei <stein-ivar@berghei.no>
2010-04-02 17:40:49 +0200

    [template] hastified

--- helper/template.lua
+++ helper/template.lua
@@ -2,5 +2,9 @@ return function(template)
 	local filename = "templates/" .. template
 	local file = io.open(filename)
 
-	if file then return file:read("*all"):sub(1, -2) end
+	if file then
+		local out = file:read("*all"):gsub('\n$', '')
+		io.close(file)
+		return out
+	end
 end
