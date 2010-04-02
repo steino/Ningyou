0cb987474c91c4420fc5e3e32d8320f6eb11f559
Stein-Ivar Berghei <stein-ivar@berghei.no>
2010-04-02 17:35:40 +0200

    [template] some cleanup

--- helper/template.lua
+++ helper/template.lua
@@ -2,9 +2,5 @@ return function(template)
 	local filename = "templates/" .. template
 	local file = io.open(filename)
 
-	if file then
-		return file:read("*all"):sub(1, -2)
-	else
-		return nil
-	end
+	if file then return file:read("*all"):sub(1, -2) end
 end
