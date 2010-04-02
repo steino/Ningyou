8fdd594077d929fd4f8d98b7228bff9c34ad57a7
Stein-Ivar Berghei <stein-ivar@berghei.no>
2010-04-02 19:39:57 +0200

    [mysql] close the require maybe :S

--- helper/mysql.lua
+++ helper/mysql.lua
@@ -1,4 +1,4 @@
-local config = loadfile(os.getenv("/home/steino/ningyou.lua")()
+local config = loadfile(os.getenv("/home/steino/ningyou.lua")())
 require"DBI"
 
 local dbh = assert(DBI.Connect('MySQL', config['db'], config['user'], config['pass'], config['host'], 3306))
