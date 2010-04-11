692b89a65567d1365497c9b2e9f903c5f8edfe5a
Stein-Ivar Berghei <stein-ivar@berghei.no>
2010-04-11 15:53:45 +0200

    [cookies] add a cookies helper

---
+++ helper/cookies.lua
@@ -0,0 +1,28 @@
+local sapi = require"helper.sapi"
+local urlcode = require"helper.urlcode"
+
+local function header(...)
+	return sapi.Response.header(...)
+end
+
+return {
+	Set = function(self, key, value, expire)
+		local cookie = key .. "=" .. urlcode:escape(value)
+		if expire then
+			local t = date("!%A, %d-%b-%Y %H:%M:%S GMT", expire)
+			cookie = cookie .. "; expires=" .. t
+		end
+		header("Set-Cookie", cookie)
+	end,
+	Get = function(self, key)
+		local cookies = os.getenv"HTTP_COOKIE"
+		cookies = ";" .. cookies .. ";"
+		cookies = cookies:gsub("%s*;%s*", ";")
+		local pattern = ";" .. key .. "=(.-);"
+		local _, _, value = cookies:find(pattern)
+		return value, urlcode:unescape(value)
+	end,
+	Delete = function(key)
+		Set(key, 1)
+	end,
+}
