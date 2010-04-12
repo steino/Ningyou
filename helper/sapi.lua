cac15ac1d05729e4fa82211905e6f1168c34ad85
Stein-Ivar Berghei <stein-ivar@berghei.no>
2010-04-13 00:02:43 +0200

    [sapi] rewrite. Loads as _G.sapi so its accessible from all helpers.

--- helper/sapi.lua
+++ helper/sapi.lua
@@ -1,35 +1,11 @@
-require"wsapi.response"
+module("sapi", package.seeall)
 
-local r = wsapi.response.new()
+headers = { }
 
-return {
-	Response = {
-		contenttype = function(header)
-			r["Content-Type"] = header
-		end,
-		errorlog = function(msg, errlevel)
-			io.stderr:write(msg)
-		end,
-		header = function(header, value)
-			if r[header] then
-				if type(r[header]) == "table" then
-					table.insert(r[header], value)
-				else
-					r[header] = { r[header], value }
-				end
-			else
-				r[header] = value
-			end
-		end,
-		redirect = function(url)
-			r.status = 302
-			r.headers["Location"] = url
-		end,
-		write = function(...)
-			r:write({...})
-		end,
-		finish = function()
-			return r:finish()
-		end,
-	}
-}
+function header(name, value)
+	if headers[name] and type(headers[name]) == "table" then 
+		table.insert(headers[name], value)
+	else
+		headers[name] = value
+	end
+end
