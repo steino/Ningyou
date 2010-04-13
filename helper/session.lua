ab5524788fa86142c23139f584e9db7e8752d9f1
Stein-Ivar Berghei <stein-ivar@berghei.no>
2010-04-13 22:53:01 +0200

    [session] make a working :New() function

--- helper/session.lua
+++ helper/session.lua
@@ -1,6 +1,37 @@
+local function find(file)
+	local fh = io.open("sessions/" .. file)
+	if fh then 
+		fh:close()
+		return true
+	end
+end
+
+local function new_id()
+	return math.random(9999999)
+end
+
+local function check_id(id)
+	return id and (id:find"^%d+$" ~= nil)
+end
+
 return {
-	Set = function(self, hash, key, value)
+	New = function(self)
+		local id = new_id()
+		if find(id .. ".lua") then
+			repeat
+				id = new_id()
+			until not find(id .. ".lua")
+			randomseed(math.mod(id, 999999999))
+		end
+		return id
 	end,
-	Unset = function(self, hash, key)
+
+	Load = function(self, id)
+	end,
+
+	Set = function(self, id, key, value)
+	end,
+
+	Unset = function(self, id, key)
 	end,
 }
