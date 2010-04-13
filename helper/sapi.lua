857ab54828cf6230196b8b382566d286178ded15
Stein-Ivar Berghei <stein-ivar@berghei.no>
2010-04-13 09:25:53 +0200

    [sapi] make it possible to add multiple headers with the same name.

--- helper/sapi.lua
+++ helper/sapi.lua
@@ -7,6 +7,10 @@ headers = {
 function header(name, value)
 	if headers[name] and type(headers[name]) == "table" then 
 		table.insert(headers[name], value)
+	elseif headers[name] then
+		local old = headers[name]
+		headers[name] = { old }
+		table.insert(headers[name], value)
 	else
 		headers[name] = value
 	end
