d772963fca120ae8d891b185d3673efb10d53ea9
Stein-Ivar Berghei <stein-ivar@berghei.no>
2010-04-14 15:16:34 +0200

    [utils] "thats not the way its done" -haste

--- helper/utils.lua
+++ helper/utils.lua
@@ -1,100 +1,105 @@
-return {
-	handleKey = function(self, key)
-		if type(key) == "number" or type(key) == "boolean" then
-			return ("[%s]"):format(key)
-		else
-			return ("[%q]"):format(tostring(key))
-		end
-	end,
-	handleValue = function(self, value, i)
-		if type(value) == "table" then
-			return self:table_tostring(value, i+1)
-		elseif type(value) == "number" or type(value) == "boolean" then
-			return value
-		else
-			return ("%q"):format(value)
-		end
-	end,
-	table_tostring = function(self, t, i)
-		if type(t) ~= "table" then return end
-		i = i or 1
-		local str = ""
-		for k,v in pairs(t) do
-			str = str .. ("%s%s = %s,\n"):format(("\t"):rep(i), self:handleKey(k), self:handleValue(v, i))
-		end
-		
-		return ("{\n"..str..("\t"):rep(i-1).."}")
-	end,
-	split = function(self, msg, char)
-		local arr = {}
-		local fchar = "(.-)" .. char
-		local last_end = 1
-		local s, e, cap = msg:find(fchar, 1)
+module("utils", package.seeall)
 
-		while s do
-			if s ~= 1 or cap ~= "" then
-				table.insert(arr, cap)
-			end
-			last_end = e+1
-			s, e, cap = msg:find(fchar, last_end)
-		end
-
-		if last_end <= #msg then
-			cap = msg:sub(last_end)
-			table.insert(arr, cap)
-		 end
-
-		return arr
-	end,
-	table_print = function(self, tt, indent, done)
-		done = done or {}
-		indent = indent or 0
-		if type(tt) == "table" then
-			for key, value in pairs (tt) do
-				io.write(string.rep (" ", indent)) -- indent it
-				if type (value) == "table" and not done [value] then
-					done [value] = true
-					io.write(string.format("[%s] => table\n", tostring (key)));
-					io.write(string.rep (" ", indent+4)) -- indent it
-					io.write("(\n");
-					self:table_print (value, indent + 7, done)
-					io.write(string.rep (" ", indent+4)) -- indent it
-					io.write(")\n");
-				else
-					io.write(string.format("[%s] => %s\n",
-					tostring (key), tostring(value)))
-				end
-			end
-		else
-			io.write(tt .. "\n")
-		end
-	end,
-	copy = function(self, a, out)
-		local o = out or {}
-		for k,v in pairs(a) do
-			o[k] = v
-		end
-		return o
-	end,
-	intersect = function(self, a, b, out)
-		local o = out or {}
-		for k,v in pairs(a) do 
-			o[k] = b[k]
-		end
-		return o
-	end,
-	union = function(self, a, b, out)
-		local o = out or {}
-		self:copy(a, o)
-		self:copy(b, o)
-		return o
-	end,
-	difference = function(self, a, b, out)
-		local o = out or {}
-		self:copy(a, o)
-		for k,v in pairs(b) do
-			o[k] = nil
-		end
-		return o
+local handleKey = function(self, key)
+	if type(key) == "number" or type(key) == "boolean" then
+		return ("[%s]"):format(key)
+	else
+		return ("[%q]"):format(tostring(key))
 	end
-}
+end
+
+local handleValue = function(self, value, i)
+	if type(value) == "table" then
+		return table_tostring(value, i+1)
+	elseif type(value) == "number" or type(value) == "boolean" then
+		return value
+	else
+		return ("%q"):format(value)
+	end
+end
+
+function table_tostring(self, t, i)
+	if type(t) ~= "table" then return end
+	i = i or 1
+	local str = ""
+	for k,v in pairs(t) do
+		str = str .. ("%s%s = %s,\n"):format(("\t"):rep(i), handleKey(k), handleValue(v, i))
+	end
+	
+	return ("{\n"..str..("\t"):rep(i-1).."}")
+end
+
+function split(self, msg, char)
+	local arr = {}
+	local fchar = "(.-)" .. char
+	local last_end = 1
+	local s, e, cap = msg:find(fchar, 1)
+		while s do
+		if s ~= 1 or cap ~= "" then
+			table.insert(arr, cap)
+		end
+		last_end = e+1
+		s, e, cap = msg:find(fchar, last_end)
+	end
+		if last_end <= #msg then
+		cap = msg:sub(last_end)
+		table.insert(arr, cap)
+	end
+	return arr
+end
+
+function table_print(self, tt, indent, done)
+	done = done or {}
+	indent = indent or 0
+	if type(tt) == "table" then
+		for key, value in pairs (tt) do
+			io.write(string.rep (" ", indent)) -- indent it
+			if type (value) == "table" and not done [value] then
+				done [value] = true
+				io.write(string.format("[%s] => table\n", tostring (key)));
+				io.write(string.rep (" ", indent+4)) -- indent it
+				io.write("(\n");
+				table_print (value, indent + 7, done)
+				io.write(string.rep (" ", indent+4)) -- indent it
+				io.write(")\n");
+			else
+				io.write(string.format("[%s] => %s\n",
+				tostring (key), tostring(value)))
+			end
+		end
+	else
+		io.write(tt .. "\n")
+	end
+end
+
+function copy(a, out)
+	local o = out or {}
+	for k,v in pairs(a) do
+		o[k] = v
+	end
+	return o
+end
+	
+function intersect(a, b, out)
+	local o = out or {}
+	for k,v in pairs(a) do 
+		o[k] = b[k]
+	end
+	return o
+end
+
+function union(a, b, out)
+	local o = out or {}
+	copy(a, o)
+	copy(b, o)
+	return o
+end
+
+function difference(a, b, out)
+	local o = out or {}
+	copy(a, o)
+	for k,v in pairs(b) do
+		o[k] = nil
+	end
+	return o
+end
