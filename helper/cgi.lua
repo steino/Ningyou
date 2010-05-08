b77e4b53b65a1dfb33e64e905d114bc4d1463190
Stein-Ivar Berghei <stein-ivar@berghei.no>
2010-05-08 17:52:05 +0200

    [cgi] add :Run()

--- helper/cgi.lua
+++ helper/cgi.lua
@@ -1,3 +1,5 @@
+if not _G.ningyou then _G.ningyou = {} end
+
 return {
 	Post = function(self, stdin, limit)
 		limit = tonumber(limit) or 0
@@ -20,5 +22,22 @@ return {
 		local out = {}
 		query:gsub("([^&=]+)=([^&=]*)&?", function(i,v) out[i] = v end)
 		return out
+	end,
+	Run = function(self, file)
+		local fh = pcall(io.open(file))
+		if not fh then return end
+		fh:close()
+
+		-- Fill POST/QUERY
+		_G.ningyou.POST = pcall(self:Post(io.stdin, os.getenv"CONTENT_LENGHT"))
+		_G.ningyou.QUERY = pcall(self:Get(os.getenv"QUERY_STRING"))
+
+		local content, err = loadfile(file)
+
+		if not content then
+			return nil, err
+		else
+			return pcall(content())
+		end
 	end,
 }
