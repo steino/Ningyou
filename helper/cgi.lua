88c7c3d243a17397c90c168a3a1dbf2f2834ad33
Stein-Ivar Berghei <stein-ivar@berghei.no>
2010-05-12 17:21:30 +0200

    [cgi] actually remove the function ...

--- helper/cgi.lua
+++ helper/cgi.lua
@@ -24,21 +24,4 @@ return {
 		query:gsub("([^&=]+)=([^&=]*)&?", function(i,v) out[i] = v end)
 		return out
 	end,
-	Run = function(self, file)
-		local fh = io.open(file)
-		if not fh then return nil, "Could not open file." end
-		fh:close()
-
-		-- Fill POST/QUERY
-		_G.ningyou.POST = pcall(self:Post(io.stdin, os.getenv"CONTENT_LENGHT"))
-		_G.ningyou.QUERY = self:Get(os.getenv"QUERY_STRING")
-
-		local content, err = loadfile(file)
-
-		if not content then
-			return nil, err
-		else
-			return pcall(content)
-		end
-	end,
 }
