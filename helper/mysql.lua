048d0613694586cfcb23b257f5db11cf5237b4d9
Stein-Ivar Berghei <stein-ivar@berghei.no>
2010-06-04 20:47:52 +0200

    [mysql] haste said we don't need the _G

--- helper/mysql.lua
+++ helper/mysql.lua
@@ -1,11 +1,11 @@
-if not _G.ningyou then _G.ningyou = {} end
+if not ningyou then ningyou = {} end
 
 require"DBI"
 local config = loadfile("/home/steino/ningyou.lua")()
 local dbh = assert(DBI.Connect('MySQL', config['db'], config['user'], config['pass'], config['host'], 3306))
 
 if dbh:ping() then
-	_G.ningyou.mysql = dbh
+	ningyou.mysql = dbh
 else
 	return nil, "Unable to connect to database"
 end
