9ce2665a243265d9791a3964ed5ec2cb3cee657f
Stein-Ivar Berghei <stein-ivar@berghei.no>
2010-06-11 09:25:37 +0200

    [dispatcher] small fixes, adding a home template/container

---
+++ containers/home.lua
@@ -0,0 +1,3 @@
+tags.Register("title", function() return "Ningyou" end)
+tags.Register("css", function() return "" end)
+tags.Register("content", function() return "<a>Coming soon&#153;</a>" end)
--- index.lua
+++ index.lua
@@ -7,16 +7,15 @@ local cgi = require"helper.cgi"
 local url = require"helper.url"
 local template = require"helper.template"
 local _ENV = os.getenv
-local _URL = url(_ENV"PATH_INFO" or "/")
+local _URL = url(_ENV"PATH_INFO" or "/home")
 local _write = io.write
 local _open = io.open
 local _load = loadfile
 
-
 ningyou.POST = cgi:Post(io.stdin, _ENV"CONTENT_LENGHT")
 ningyou.QUERY = cgi:Get(_ENV"QUERY_STRING")
 
-local file = "containers/"..(_URL[1] or "")..".lua" or ""
+local file = "containers/"..(_URL[1] or "")..".lua"
 local openfile, fh = pcall(_open, file)
 
 if openfile and fh then
