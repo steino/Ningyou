9af44405a26897d28b103e8bd5e5904e39a3b0de
Stein-Ivar Berghei <stein-ivar@berghei.no>
2010-06-20 20:58:56 +0200

    [dispatcher] make use of the crash helper

--- index.lua
+++ index.lua
@@ -2,6 +2,7 @@
 
 require"helper.sapi" -- Server API stuff.
 require"helper.tags" -- For reading and parsing ningyou tags.
+require"helper.crash"
 
 local cgi = require"helper.cgi"
 local url = require"helper.url"
@@ -12,45 +13,34 @@ local _write = io.write
 local _open = io.open
 local _load = loadfile
 
+local page = (_URL[1] or "home")
+
 ningyou.POST = cgi:Post(io.stdin, _ENV"CONTENT_LENGHT")
 ningyou.QUERY = cgi:Get(_ENV"QUERY_STRING")
 
-local file = "containers/"..(_URL[1] or "home")..".lua"
+local file = "containers/".. page ..".lua"
 local openfile, fh = pcall(_open, file)
 
 if openfile and fh then
 	fh:close()
 else
-	pcall(sapi.setheader)
-	if ningyou.mysql then ningyou.mysql:close() end
-	return _write"404"
+	return _write(crash.error"404")
 end
 
 local _, run, err = pcall(_load, file)
 
-if err then 
-	pcall(sapi.setheader)
-	if ningyou.mysql then ningyou.mysql:close() end
-	-- Add some kind of debug page here.
-	return _write(err) 
+if err then return _write(crash.error("", err, page)) end
+
+if not pcall(run) then 
+	return _write(crash.error("", "Something terribly wrong happened", page))
 end
 
-if not pcall(run) then
-	pcall(sapi.setheader)
-	if ningyou.mysql then ningyou.mysql:close() end
-	return _write"Something terribly wrong happened"
-end
+local _, content = pcall(tags.Render, template(page))
 
-local _, content = pcall(tags.Render, template(_URL[1] or "home"))
+pcall(sapi.setheader)
 
-if not pcall(sapi.setheader) then
-	if ningyou.mysql then ningyou.mysql:close() end
-	return _write"Unable to set headers."
-end
-
-if not pcall(_write, content) then
-	if ningyou.mysql then ningyou.mysql:close() end
-	return _write"Something terribly wrong happened"
+if not pcall(_write, content) then 
+	return _write(crash.error("", "Something terribly wrong happened", page))
 end
 
 if ningyou.mysql then ningyou.mysql:close() end
