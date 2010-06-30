3a2c03648d58733f494d375225fe7b19ad71bd6b
Trond A Ekseth <troeks@gmail.com>
2010-06-30 20:03:24 +0200

    Remove some locals.

--- index.lua
+++ index.lua
@@ -14,35 +14,32 @@ local url = require"helper.url"
 local template = require"helper.template"
 local _ENV = os.getenv
 local _URL = url(_ENV"PATH_INFO" or "/")
-local _write = io.write
-local _open = io.open
-local _load = loadfile
 
 local page = (_URL[1] or "home")
 
 local file = "containers/".. page ..".lua"
-local openfile, fh = pcall(_open, file)
+local openfile, fh = pcall(io.open, file)
 
 if openfile and fh then
 	fh:close()
 else
-	return _write(crash.error(404))
+	return io.write(crash.error(404))
 end
 
-local _, run, err = pcall(_load, file)
+local _, run, err = pcall(loadfile, file)
 
-if err then return _write(crash.error(err, page)) end
+if err then return io.write(crash.error(err, page)) end
 
 if not pcall(run) then
-	return _write(crash.error("Something terribly wrong happened", page))
+	return io.write(crash.error("Something terribly wrong happened", page))
 end
 
 local _, content = pcall(tags.Render, template((ningyou.template or page)))
 
 pcall(sapi.setheader)
 
-if not pcall(_write, content) then
-	return _write(crash.error("Something terribly wrong happened", page))
+if not pcall(io.write, content) then
+	return io.write(crash.error("Something terribly wrong happened", page))
 end
 
 if ningyou.mysql then ningyou.mysql:close() end
