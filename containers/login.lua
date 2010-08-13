d7b0f522d4fdea3566c817f7db1edf45760c14c5
Stein-Ivar Berghei <stein-ivar@berghei.no>
2010-08-14 00:04:26 +0200

    Fix login stuff

--- containers/login.lua
+++ containers/login.lua
@@ -1,8 +1,15 @@
 local cgi = require"helper.cgi"
-local post = cgi:Post(io.stdin, _ENV("CONTENT_LENGHT"))
+local template = require"helper.template"
+local post = cgi:Post(io.stdin, os.getenv"CONTENT_LENGHT")
 
 ningyou.template = "default"
 
 tags.Register("title", function() return "Ningyou" end)
 tags.Register("css", function() return "" end)
-tags.Register("content", function() return post["login"] end)
+tags.Register("content", function() 
+	if post then
+		return post["login"]
+	else
+		return tags.Render(template"login")
+	end
+end)
