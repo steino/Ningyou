98cca20c9c69b179c9baf8b353257d631610f115
Stein-Ivar Berghei <stein-ivar@berghei.no>
2010-08-13 23:53:57 +0200

    Change some login stuff

--- containers/login.lua
+++ containers/login.lua
@@ -1,2 +1,8 @@
+local cgi = require"helper.cgi"
+local post = cgi:Post(io.stdin, _ENV("CONTENT_LENGHT"))
+
+ningyou.template = "default"
+
 tags.Register("title", function() return "Ningyou" end)
 tags.Register("css", function() return "" end)
+tags.Register("content", function() return post["login"] end)
