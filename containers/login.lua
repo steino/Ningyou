1ccd13440bc259dedcb39358f01ec06a63502023
Stein-Ivar Berghei <stein-ivar@berghei.no>
2010-08-14 15:33:25 +0200

    Make login actually work

--- containers/login.lua
+++ containers/login.lua
@@ -1,6 +1,8 @@
+local auth = require"helper.auth"
 local cgi = require"helper.cgi"
 local template = require"helper.template"
 local post = cgi:Post(io.stdin, os.getenv"CONTENT_LENGTH")
+local check = auth:check()
 
 ningyou.template = "default"
 
@@ -8,7 +10,14 @@ tags.Register("title", function() return "Ningyou" end)
 tags.Register("css", function() return "" end)
 tags.Register("content", function() 
 	if post then
-		return post["login"]
+		local user = auth:login(post["login"], post["pw"])
+		if user then
+			return "Successfully logged in as: " .. user
+		else
+			return "Wrong username or password, try <a href='login'> again</a>"
+		end
+	elseif check then
+		return "Already logged in as: " .. check
 	else
 		return tags.Render(template"login")
 	end
