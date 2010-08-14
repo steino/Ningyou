1bdb4570e9ef837007e7a64546bf00dc6541b243
Stein-Ivar Berghei <stein-ivar@berghei.no>
2010-08-14 18:18:20 +0200

    Use errormsg provided by auth helper

--- containers/login.lua
+++ containers/login.lua
@@ -10,15 +10,15 @@ tags.Register("title", function() return "Ningyou" end)
 tags.Register("css", function() return "" end)
 tags.Register("content", function() 
 	if post then
-		local user = auth:login(post["login"], post["pw"])
+		local user, loginerr = auth:login(post["login"], post["pw"])
 		if user then
 			return "Successfully logged in as: " .. user
 		else
-			return "Wrong username or password, try <a href='login'> again</a>"
+			return "Could not log you in because: " .. loginerr .. "<br/><br/><a href='login'>Try again.</a>"
 		end
 	elseif check then
 		return "Already logged in as: " .. check
 	else
-		return tags.Render(template"login")
+		return tags.Render(template"login") .. _ENV"HTTP_COOKIE"
 	end
 end)
