07ee64baf511b09cc496cd88646d897d2978a198
Stein-Ivar Berghei <stein-ivar@berghei.no>
2010-10-10 02:25:44 +0200

    Updated anidb import helper, now does everything on its own

--- helper/anidb_import.lua
+++ helper/anidb_import.lua
@@ -1,31 +1,47 @@
+local x = os.clock()
+
 require"helper.mysql"
 
+print"Downloading latest titles file from anidb.net:"
+os.execute"wget -N http://anidb.net/api/animetitles.dat.gz"
+
+print"Extracting file:"
+os.execute"zcat -v animetitles.dat.gz > animetitles.dat"
+
 local db = ningyou.mysql
-local titles = loadfile(os.getenv("HOME").."/animeTitles.lua")()
-local up, ins = 0, 0
-local updates = {}
-local update_count = 0
+local up, ins, up_c, updates = 0, 0, 0, {}
 local run_update, error_update, run_import, error_import
 
-local check = db:prepare('select animeid, language, title from nin_titles_anime')
-local import, im_error = db:prepare('insert into nin_titles_anime (animeid, title, language) values (?,?,?)')
-local update, up_error = db:prepare('update nin_titles_anime set title = ?, language = ? where animeid = ?')
-local history = db:prepare("insert into nin_history (userid, showtype, showid, event, value) values (?,?,?,?,?)")
+local check = db:prepare"select animeid, language, title from nin_titles_anime"
+local import, im_error = db:prepare"insert into nin_titles_anime (animeid, title, language) values (?,?,?)"
+local update, up_error = db:prepare"update nin_titles_anime set title = ?, language = ? where animeid = ?"
+local history = db:prepare"insert into nin_history (userid, showtype, showid, event, value) values (?,?,?,?,?)"
 
 check:execute()
-db:commit()
+
+io.write"\nParsing file: "
+
+local titles = loadfile"parse.lua"()
+
+print"Done."
+
+io.write"\nChecking for existing titles in the database: "
 
 for row in check:rows(true) do
 	if not updates[row["animeid"]] then updates[row["animeid"]] = {} end
 	if titles[row["animeid"]] then
 		if titles[row["animeid"]][row["language"]] then
 			updates[row["animeid"]][row["language"]] = row["title"]
-			update_count = update_count + 1
+			up_c = up_c + 1
 		end
 	end
 end
 
 check:close()
+
+print(up_c .. " titles found.")
+
+io.write"Inserting and updating titles: "
 
 for id, t in pairs(titles) do
 	for lang, title in pairs(t) do
@@ -45,16 +61,18 @@ for id, t in pairs(titles) do
 	end
 end
 
+print(up .. " updated, " .. ins .. " inserted.")
+
 run_history, error_history = history:execute("admin", "anime", "NULL", "import_anidb_titles", ins.. ";"..up)
 
+io.write"Commiting changes to database: "
 db:commit()
-
-print("There is ".. update_count .. " titles in the update table.")
-print("Inserted ".. ins .." rows.")
-print("Updated " .. up .." rows.")
+print"Done."
 
 import:close()
 update:close()
 up, ins = nil, nil
 updates = nil
 update_count = nil
+
+print(string.format("Elapsed time: %.2f", os.clock() - x))
